5e82fd2382efc53fab6ceb057189da07e741ccd0
Alex Krolick <akrolick@box.com>
2019-10-25 14:22:10 -0700

    test(tooltip): fix module state in tests

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,15 @@ module.exports = {
   transform: {
     "^.+\\.jsx?$": "./jest-transformer.js"
   },
+  // Set resetMocks: true
+  // This clears mock state + implementation between each test() statement
   resetMocks: true,
-  resetModules: true,
+  // Set resetModules: false
+  // Setting to true will cause issues with React Testing Libray cleanup &
+  // React instances for Hooks
+  // Manually isolate instead, if needing to clear module state between tests
+  // https://jestjs.io/docs/en/jest-object#jestisolatemodulesfn
+  resetModules: false,
   globals: {
     __DEV__: true
   }
--- packages/tooltip/src/index.test.js
+++ packages/tooltip/src/index.test.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { render, fireEvent, act } from "@testing-library/react";
+import * as autoId from "@reach/auto-id"; // eslint-disable-line no-unused-vars
 import Tooltip, { LEAVE_TIMEOUT, MOUSE_REST_TIMEOUT } from ".";
+
+jest.mock("@reach/auto-id", () => ({
+  ...jest.requireActual("@reach/auto-id")
+}));
 
 jest.mock("@reach/utils", () => ({
   ...jest.requireActual("@reach/utils"),
   checkStyles: jest.fn()
 }));
 
-describe("Tooltip", () => {
-  beforeEach(() => {
-    jest.useFakeTimers();
-  });
+beforeEach(() => {
+  const newAutoId = jest.requireActual("@reach/auto-id");
+  jest.setMock("@reach/auto-id", newAutoId);
+  jest.useFakeTimers();
+});
 
+describe("Tooltip", () => {
   it("shows/hides on hover", () => {
     const { baseElement, getByText } = render(
       <Tooltip label="Content">
@@ -20,13 +27,13 @@ describe("Tooltip", () => {
     );
     const trigger = getByText("Trigger");
 
-    expect(baseElement).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot("closed tooltip");
     fireEvent.mouseOver(trigger);
     act(() => jest.advanceTimersByTime(MOUSE_REST_TIMEOUT));
-    expect(baseElement).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot("after mouse enter");
     fireEvent.mouseLeave(trigger);
     act(() => jest.advanceTimersByTime(LEAVE_TIMEOUT));
-    expect(baseElement).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot("after mouse leave");
   });
 
   it("shows/hides when trigger is activeElement", () => {
@@ -37,12 +44,12 @@ describe("Tooltip", () => {
     );
     const trigger = getByText("Trigger");
 
-    expect(baseElement).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot("before focus");
     fireEvent.focus(trigger);
-    expect(baseElement).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot("after focus");
     fireEvent.blur(trigger);
     act(() => jest.advanceTimersByTime(LEAVE_TIMEOUT));
-    expect(baseElement).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot("after blur");
   });
 
   it("shows without timeout when one tooltip is already visible", () => {
@@ -61,10 +68,10 @@ describe("Tooltip", () => {
 
     fireEvent.mouseOver(firstTrigger);
     act(() => jest.advanceTimersByTime(MOUSE_REST_TIMEOUT));
-    expect(baseElement).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot("after first mouseover");
     fireEvent.mouseLeave(firstTrigger);
     fireEvent.mouseOver(secondTrigger);
-    expect(baseElement).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot("after second mouseover");
   });
 
   it("hides on ESC", () => {
@@ -77,8 +84,8 @@ describe("Tooltip", () => {
     const trigger = getByText("Trigger");
     fireEvent.focus(trigger);
     act(() => jest.advanceTimersByTime(MOUSE_REST_TIMEOUT));
-    expect(baseElement).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot("after focus");
     fireEvent.keyDown(trigger, { key: "Escape" });
-    expect(baseElement).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot("after escape");
   });
 });
